5f87889c65ee3dde357d56ace6c99ccff7bd0028
Danish <itsaws786@gmail.com>
2024-09-14 21:31:54 +0530

    mongoose code added some

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express=require("express")
-
-const a=prompt("enter the no")
-console.log(a)
+const mongoose=require("mongoose")
 const app=express();
+
+mongoose.connect("mongodb://localhost:27017/danish").then(()=>{
+    console.log("Connected to mongodb")
+})
+
+const schema=new mongoose.Schema({
+    name:String,
+    password:String
+})
+
+const createdModel=mongoose.model('Danish-auth',schema)
+
+const doc=new createdModel({
+    name:"danish",
+    password:"10101"
+})
+
+doc.save()
+
 
 app.get('/',(req,res)=>{
     res.send("Home Page visited")
+})
+
+app.get('/getData',async(req,res)=>{
+    const docs=await createdModel.findOne()
+    console.log(docs)
 })
 
 app.listen(3000)
