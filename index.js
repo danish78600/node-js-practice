0326d5954bc679a92626687c1c7a9a761e239e0d
Danish <itsaws786@gmail.com>
2024-09-15 13:02:38 +0530

    Login and Singup pages initiated

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser=require('body-parser')
+const app = express();
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded())
+app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/danish").then(() => {
+  console.log("Connected to mongodb");
+});
 
-mongoose.connect("mongodb://localhost:27017/danish").then(()=>{
-    console.log("Connected to mongodb")
+const schema = new mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+const createdModel = mongoose.model("Danish-auth", schema);
+
+// const doc = new createdModel({
+//   name: "danish",
+//   password: "10101",
+// });
+
+// doc.save();
+
+// app.get("/getData", async (req, res) => {
+//   const docs = await createdModel.findOne();
+//   res.send(docs);
+// });
+
+app.get("/", (req, res) => {
+  res.render('index')
+});
+
+
+
+app.post("/addData", (req, res) => {
+  const docs = new createdModel({
+    name: req.body.name,
+    password: req.body.password,
+  });
+  docs.save()
+  res.send("data added")
+});
+
+app.post('/login',async(req,res)=>{
+    const findUser= await createdModel.findOne({name:req.body.name,password:req.body.password})
+    if(findUser)
+    {
+        res.send(findUser)
+    }
+    else{
+        res.send("no user found")
+    }
 })
 
-const schema=new mongoose.Schema({
-    name:String,
-    password:String
-})
 
-const createdModel=mongoose.model('Danish-auth',schema)
-
-const doc=new createdModel({
-    name:"danish",
-    password:"10101"
-})
-
-doc.save()
-
-
-app.get('/',(req,res)=>{
-    res.send("Home Page visited")
-})
-
-app.get('/getData',async(req,res)=>{
-    const docs=await createdModel.findOne()
-    console.log(docs)
-})
-
-app.listen(3000)
+app.listen(3000);
